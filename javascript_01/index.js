885e2f885168369826bb0586fdc0d97afb895e96
Federico Soria <lfede.soria@gmail.com>
2019-09-18 18:47:20 -0300

    tarea java scrip

---
+++ javascript_01/index.js
@@ -0,0 +1,26 @@
+var user = [
+    {
+        name: 'Federico',
+        lastname: 'Soria',
+        email: 'lfede.soria@gamil.com',
+        birthday: new Date().toLocaleDateString(),
+        age: '30',
+    },
+    {
+        name: 'Joaquin',
+        lastname: 'Soria',
+        email: 'joaquin.soria@gmail.com',
+        birthday: '24/12/96',
+        age: '23',
+    },
+    {
+        name: 'Luis',
+        lastname: 'Vasquez',
+        email: 'lucho-vasquez@gmail.com',
+        birthday: '12/01/90',
+        age: '29',
+    },
+];
+
+console.log(user);
+
